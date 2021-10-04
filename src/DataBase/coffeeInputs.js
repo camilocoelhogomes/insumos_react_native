0e4ee08c929f32a59d4a8712a6b8d892c9830281
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-10-04 11:18:42 -0300

    rendering the inputs dinamicaly

---
+++ src/DataBase/coffeeInputs.js
@@ -0,0 +1,42 @@
+const macroNutrientsInputs = [
+    {
+        value: 35,
+        unit: 'sc/ha',
+        label: 'Produtividade Esperada',
+        item: 'productivity'
+    },
+    {
+        value: 3,
+        unit: 'm',
+        label: 'Distância entre Linhas',
+        item: 'distanceLines'
+    },
+    {
+        value: 1,
+        unit: 'm',
+        label: 'Distância entre Plantas',
+        item: 'distancePlants'
+    },
+    {
+        value: 20,
+        unit: 'ºC',
+        label: 'Temperatura média anual',
+        item: 'temperature'
+    },
+    {
+        value: 8,
+        unit: 'P mg/dm³',
+        label: 'Fósforo',
+        item: 'phosphor'
+    },
+    {
+        value: 80,
+        unit: 'K mg/dm³',
+        label: 'Potássio',
+        item: 'potassium'
+    }
+]
+
+export {
+    macroNutrientsInputs
+}
---
+++ src/components/InputField.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Text, TouchableHighlight } from 'react-native';
+
+const InputField = ({ macroNutrient }) => {
+    const {
+        value,
+        unit,
+        label,
+        item
+    } = macroNutrient
+    return (
+        <View style={localStyle.main}>
+            <Text>{label}</Text>
+            <TextInput
+                placeholder={unit}
+                keyboardType='numeric'
+            />
+            <View>
+                <TouchableHighlight >
+                    <View >
+                        <Text>Mais</Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight >
+                    <View >
+                        <Text>Menos</Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+        </View>
+    )
+}
+
+export default InputField
+
+const localStyle = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        backgroundColor: 'red',
+        alignSelf: "stretch"
+    }
+})
