ef87ec55582a894361a2d9ce9353c41f14daff5c
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-10-05 11:31:33 -0300

    get values dinamic

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, TextInput } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
